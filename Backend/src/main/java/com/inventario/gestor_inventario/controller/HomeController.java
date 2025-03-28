7184cf81bc228b64311036420896cddcf25d160f
Fran Santana <santanafran96@gmail.com>
2025-03-28 08:14:05 +0100

    solucionado clase doble HomeController

--- Backend/src/main/java/com/inventario/gestor_inventario/controller/HomeController.java
+++ Backend/src/main/java/com/inventario/gestor_inventario/controller/HomeController.java
@@ -2,11 +2,7 @@ package com.inventario.gestor_inventario.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class HomeController {
@@ -15,19 +11,4 @@ public class HomeController {
         return "index";
     }
 }
-=======
->>>>>>> b4d5592508785c4c22c3439c802a6da2dbb0eaf5
 
-@Controller
-public class HomeController {
-
-    @GetMapping(value="/home")
-    public String home() {
-        return "index";  // JSP sin la extensión .jsp
-    }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> rama_manuel
->>>>>>> b4d5592508785c4c22c3439c802a6da2dbb0eaf5
