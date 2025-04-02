b519f9fa4ea58dd94c18a0a9f839b82975b1bf9e
Fran Santana <santanafran96@gmail.com>
2025-04-02 14:02:40 +0200

    añadida configuración CORS para peticiones FrontEnd

---
+++ Backend/src/main/java/com/inventario/gestor_inventario/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.inventario.gestor_inventario.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Clase de configuración para permitir mediante CORS
+ * las distintas peticiones del frontend
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
--- Backend/src/main/java/com/inventario/gestor_inventario/controller/TareaController.java
+++ Backend/src/main/java/com/inventario/gestor_inventario/controller/TareaController.java
@@ -3,6 +3,8 @@ package com.inventario.gestor_inventario.controller;
 import com.inventario.gestor_inventario.entities.Tarea;
 import com.inventario.gestor_inventario.repository.TareaRepository;
 import com.inventario.gestor_inventario.service.implementations.TareaServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tarea")
 public class TareaController {
+    private static final Logger logger = LoggerFactory.getLogger(TareaController.class);
 
     private TareaServiceImpl repository;
 
@@ -21,9 +24,16 @@ public class TareaController {
 
     @GetMapping
     public List<Tarea> ListarTarea() {
-        return this.repository.ListarTarea();
+        logger.info("Iniciando método ListarTarea()");
+        try {
+            List<Tarea> tareas = this.repository.ListarTarea();
+            logger.info("Se obtuvieron {} tareas", tareas.size());
+            return tareas;
+        } catch (Exception e) {
+            logger.error("Error al listar tareas: {}", e.getMessage(), e);
+            throw e; // Puedes personalizar el manejo si usas un handler global
+        }
     }
-
     @PostMapping
     public Tarea CrearTarea(@RequestBody Tarea tarea) {
         return this.repository.CrearTarea(tarea);
