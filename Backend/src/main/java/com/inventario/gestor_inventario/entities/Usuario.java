14ad238d19238839f4ab8d87170d76698737eb4f
Fran Santana <santanafran96@gmail.com>
2025-04-02 09:45:19 +0200

    arreglada clase usuario y añadida alguna validación en el registro a falta de añadir más

--- Backend/src/main/java/com/inventario/gestor_inventario/entities/Usuario.java
+++ Backend/src/main/java/com/inventario/gestor_inventario/entities/Usuario.java
@@ -1,21 +1,17 @@
 package com.inventario.gestor_inventario.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.inventario.gestor_inventario.utilities.ProductoIdSerializer;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+
 import java.util.Date;
-import java.util.HashSet;
+
 import java.util.Optional;
-import java.util.Set;
+
 
 @Entity
 @Table(name = "usuarios")
@@ -27,7 +23,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id_usuario;
-    private String nombre,contraseña;
+    private String nombre,contrasena;
     @Column(unique = true)
     private String email;
     private int estado;
@@ -42,69 +38,6 @@ public class Usuario {
     }
 
 
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public int getId_usuario() {
-        return id_usuario;
-    }
-
-    public void setId_usuario(int id_usuario) {
-        this.id_usuario = id_usuario;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
-
-    public Date getFecha_baja() {
-        return fecha_baja;
-    }
-
-    public void setFecha_baja(Date fecha_baja) {
-        this.fecha_baja = fecha_baja;
-    }
-
-    public Date getFechaAlta() {
-        return fechaAlta;
-    }
-
-    public void setFechaAlta(Date fechaAlta) {
-        this.fechaAlta = fechaAlta;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
 
 
 }
