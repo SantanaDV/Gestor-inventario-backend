1fb20d86a7fcb27df94a5af205857ef95c1d2c67
Monica-74 <monica.rfid@gmail.com>
2025-04-10 14:07:25 +0200

    Arreglo

--- Backend/src/main/java/com/inventario/gestor_inventario/security/SecurityConfig.java
+++ Backend/src/main/java/com/inventario/gestor_inventario/security/SecurityConfig.java
@@ -49,6 +49,11 @@ public class SecurityConfig {
                         .requestMatchers("/tareasWeb").permitAll()
                         .requestMatchers("/productosWeb").permitAll()
                         // Cualquier otra ruta requiere autenticación
+                        .requestMatchers("/login", "/registro").permitAll()
+                        .requestMatchers("/api/usuario/admin").hasRole("ADMIN")
+                        .requestMatchers("/api/usuario/**").hasAnyRole("ADMIN", "EMPLEADO")
+                        .requestMatchers("/api/usuario/empleado").hasRole("EMPLEADO")
+                        .anyRequest().authenticated()
                 )
                 .addFilter(jwtAuthenticationFilter) // Añade el filtro de autenticación JWT
                 .addFilterAfter(jwtValidationFilter, JwtAuthenticationFilter.class); // Añade el filtro de validación JWT después del filtro de autenticación
@@ -102,7 +107,73 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-
-
 }
+
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final UserDetailsService userDetailsService;
+//
+//    // Constructor para inyectar el UserDetailsService
+//    public SecurityConfig(UserDetailsService userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
+//
+//    // Configuración principal de seguridad
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+//                                                   JwtAuthenticationFilter jwtAuthenticationFilter,
+//                                                   JwtValidationFilter jwtValidationFilter) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Deshabilita la protección CSRF
+//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Configura la política de sesión como STATELESS
+//                .authorizeHttpRequests(auth -> auth
+//                        // Rutas públicas (acceso sin autenticación)
+//                        .requestMatchers("/login").permitAll() // Permitir acceso al endpoint de login
+//                        .requestMatchers("/registro").permitAll() // Permitir acceso al endpoint de registro
+////                        .requestMatchers("/api/usuario/listar").permitAll()
+//
+//                        // Cualquier otra ruta requiere autenticación
+//                        .anyRequest().authenticated()
+//                )
+//                .addFilter(jwtAuthenticationFilter) // Añade el filtro de autenticación JWT
+//                .addFilterAfter(jwtValidationFilter, JwtAuthenticationFilter.class); // Añade el filtro de validación JWT después del filtro de autenticación
+//
+//        return http.build(); // Construye y devuelve la configuración de seguridad
+//    }
+//
+//
+//    // Configura el proveedor de autenticación
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(userDetailsService); // ya es UserDetailsService
+//        provider.setPasswordEncoder(passwordEncoder());
+//        return provider;
+//    }
+//
+//    // Configura el AuthenticationManager
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+//            throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//    // Configura el codificador de contraseñas (BCrypt)
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    // Declara el filtro de autenticación JWT como un bean
+//    @Bean
+//    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+//        return new JwtAuthenticationFilter(authenticationManager);
+//    }
+//
+//    // Declara el filtro de validación JWT como un bean
+//    @Bean
+//    public JwtValidationFilter jwtValidationFilter(AuthenticationManager authenticationManager) {
+//        return new JwtValidationFilter(authenticationManager);
+//    }
+//}
